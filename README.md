b690feb876907c2613a4775c730d2dcc21c63215
Brian Pitts <meded-macbookpro@meded-macbookpro.local>
2019-01-19 00:03:13 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Galina's Fabulous Flower Clicky Game
+
+#### Test your memory with your favorite flowers from _My Garden_.
+
+#### Click on each character once to win the game. But don't click on a character twice, or it's game over for you!
+
+#### Built with React. Check it out [here]().
+
+![screenshot](public/screenshot.png)
---
+++ src/App.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import FlowerCard from "./components/FlowerCard";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./Container";
+import Row from "./Row";
+import Column from "./Column";
+import flowers from "./flowers.json";
+import "./App.css";
+
+function shuffleFlowers(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+class App extends Component {
+  // Set this.state
+  state = {
+    flowers,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: []
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    } else if (newScore === 12) {
+      this.setState({ rightWrong: "You win, my Rose!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Weeds!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledFlowers = shuffleFlowers(flowers);
+    this.setState({ flowers: shuffledFlowers });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Nav
+          title="Fabulous Flower Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          rightWrong={this.state.rightWrong}
+        />
+
+        <Title>
+          Try to click on each flower, but don't hit any duplicates, or some
+          weeds might grow!!!
+        </Title>
+
+        <Container>
+          <Row>
+            {this.state.flowers.map(flower => (
+              <Column size="md-3 sm-6">
+                <FlowerCard
+                  key={flower.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={flower.id}
+                  image={flower.image}
+                />
+              </Column>
+            ))}
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
